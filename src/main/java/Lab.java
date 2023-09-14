9be1349994697f1ce198cfc7947e84ad2d293d2a
ted balashov <theodore.balashov@revature.com>
2023-09-14 14:37:59 -0400

    lab

---
+++ src/main/java/Lab.java
@@ -0,0 +1,9 @@
+public class Lab {
+    /**
+     * @param nums a list of ints.
+     * @return a list of ints that is in the reversed order of nums.
+     */
+    public int[] reverse(int[] nums){
+        return null;
+    }
+}
